cf07aecbc0b374780f0727757cd1a14e5a4aef4f
Chloe <yunyunya@gmail.com>
2022-10-13 21:03:58 +0200

    1920. Build Array from Permutation

--- algorithms/easy/build_array_from_permutation.py
+++ algorithms/easy/build_array_from_permutation.py
@@ -4,8 +4,12 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[i] for i in nums]
+        # return [nums[i] for i in nums]
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
 
+        return ans
         
 sol = Solution().buildArray(nums = [0,2,1,5,3,4])
 print (sol)
