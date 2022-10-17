5f0bfcbab16f0bcace362a8b075556f10e2a3b5b
Chloe <yunyunya@gmail.com>
2022-10-17 21:04:44 +0200

    509. Fibonacci Number

---
+++ algorithms/easy/fibonacci_number.py
@@ -0,0 +1,22 @@
+# 509. Fibonacci Number
+
+class Solution:
+    # def fib(self, n: int) -> int:
+    #     if n == 0:
+    #         return 0
+    #     if n == 1:
+    #         return 1 
+    #     return self.fib(n-1) + self.fib(n-2)
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        
+        n1, n2 = 0, 1
+        for i in range(1, n):
+            n1, n2 = n2, n1 + n2
+
+        return n2
+
+
+sol = Solution().fib(n = 15)
+print(f'sol={sol}')
