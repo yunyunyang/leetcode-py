4ace2ca4af96ab630aa6ee41bc524da026fa7eff
Chloe <yunyunya@gmail.com>
2023-07-07 12:39:50 +0200

    206. Reverse Linked List

---
+++ algorithms/easy/reverse_linked_list.py
@@ -0,0 +1,25 @@
+# 206. Reverse Linked List
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+            
+        return prev
+
+# head = [1,2,3,4,5]
+head = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5)))))
+sol = Solution().reverseList(head)
+print(sol)
