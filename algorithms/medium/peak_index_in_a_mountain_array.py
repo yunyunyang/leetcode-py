d9daf21621ccc649dbb1c0d15368d956978f29ea
Chloe <yunyunya@gmail.com>
2023-07-25 10:46:50 +0200

    852. Peak Index in a Mountain Array

--- algorithms/medium/peak_index_in_a_mountain_array.py
+++ algorithms/medium/peak_index_in_a_mountain_array.py
@@ -6,6 +6,7 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         l, r = 0, len(arr)-1
         m = len(arr)//2
+        
         while l < r:
             m = l + (r-l)//2
             if arr[m] > arr[m-1] and arr[m] > arr[m+1]:
