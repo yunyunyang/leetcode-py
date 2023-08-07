e715adff50ddba4b83d33b674b1534e4c45899f4
Chloe <yunyunya@gmail.com>
2023-08-07 12:38:31 +0200

    1768. Merge Strings Alternately

--- algorithms/easy/merge_strings_alternately.py
+++ algorithms/easy/merge_strings_alternately.py
@@ -2,14 +2,13 @@
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        output = ''
+        o = ""
         s = min(len(word1), len(word2))
         for i in range(s):
-            output += word1[i] + word2[i]
-            
-        output += word1[s:] + word2[s:]
-        return output
+            o += word1[i] + word2[i]
+        o += word1[s:] + word2[s:]
 
+        return o
 
 sol = Solution().mergeAlternately(word1 = "abc", word2 = "pqr")
 print(sol)
