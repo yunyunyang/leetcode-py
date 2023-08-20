f54183ccb6075b7d529fc210af246a0cc120759c
Chloe <yunyunya@gmail.com>
2023-08-20 11:23:08 +0200

    36. Valid Sudoku

---
+++ algorithms/medium/valid_sudoku.py
@@ -0,0 +1,37 @@
+# 36. Valid Sudoku
+
+from typing import List
+import collections
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        boxes = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r//3, c//3)]:
+                    return False
+                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxes[(r//3, c//3)].add(board[r][c])
+
+        print(boxes)
+
+        return True
+    
+sol = Solution().isValidSudoku(board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]])
+print(sol)
