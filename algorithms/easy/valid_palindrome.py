6154db5bc582bf2e06adc993611fdad040a3461d
Chloe <yunyunya@gmail.com>
2023-09-15 17:59:56 +0200

    125. Valid Palindrome

---
+++ algorithms/easy/valid_palindrome.py
@@ -0,0 +1,27 @@
+# 125. Valid Palindrome
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9")
+        )
+
+
+sol = Solution().isPalindrome(s="A man, a plan, a canal: Panama")
+print(sol)
