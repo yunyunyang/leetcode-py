0d69f1c199e118a2bc187e4bb0d9534919a7171c
Chloe <yunyunya@gmail.com>
2023-10-10 15:33:14 +0200

    136. Single Number

--- algorithms/easy/single_number.py
+++ algorithms/easy/single_number.py
@@ -2,22 +2,14 @@
 
 from typing import List
 
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            counter = {}
-            for i in nums:
-                if i in counter:
-                    counter.update({i: counter.get(i)+1})
-                else:
-                    counter.update({i: 1})
-
-            for k, v in counter.items():
-                if v == 1:
-                    return k
+        o = 0
+        for n in nums:
+            o = o ^ n
+        return o
 
 
-sol = Solution().singleNumber(nums = [2,2,1])
+sol = Solution().singleNumber(nums=[2, 2, 1])
 print(sol)
