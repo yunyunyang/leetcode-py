7775ab0903dba6e08602a319f99d4009cf6e6c66
Chloe <yunyunya@gmail.com>
2023-10-17 16:55:02 +0200

    844. Backspace String Compare

---
+++ algorithms/easy/backspace_string_compare.py
@@ -0,0 +1,24 @@
+# 844. Backspace String Compare
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1, s2 = [], []
+        for i in s:
+            if i == "#":
+                if s1:
+                    s1.pop()
+            else:
+                s1.append(i)
+
+        for i in t:
+            if i == "#":
+                if s2:
+                    s2.pop()
+            else:
+                s2.append(i)
+
+        return "".join(s1) == "".join(s2)
+
+
+sol = Solution().backspaceCompare(s="y#fo##f", t="y#f#o##f")
+print(sol)
