73592ae91b947b12cf7d62641e47d74d1e29b0ca
Chloe <yunyunya@gmail.com>
2023-10-23 15:46:46 +0200

    342. Power of Four

--- algorithms/easy/power_of_four.py
+++ algorithms/easy/power_of_four.py
@@ -2,9 +2,19 @@
 
 import math
 
+
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return n > 0 and math.log(n, 4).is_integer()
+        if (n > 0) and (n & (n - 1) == 0):
+            m = 0
+            while n > 1:
+                m += 1
+                n >>= 1
+            return m % 2 == 0
+        return False
 
-sol = Solution().isPowerOfFour(n = -64)
+        # return n > 0 and math.log(n, 4).is_integer()
+
+
+sol = Solution().isPowerOfFour(n=64)
 print(sol)
