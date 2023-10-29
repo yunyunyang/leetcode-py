1c68c45ddf1b4f0478762bd8b151755ee962f21b
Chloe <yunyunya@gmail.com>
2023-10-29 19:44:25 +0100

    2744. Find Maximum Number of String Pairs

--- algorithms/easy/find_maximum_number_of_string_pairs.py
+++ algorithms/easy/find_maximum_number_of_string_pairs.py
@@ -2,15 +2,20 @@
 
 from typing import List
 
+
 class Solution:
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
-        o = 0
-        for i in range(len(words)):
-            for j in range(i+1, len(words)):
-                if words[i] == words[j][::-1]:
-                    o += 1
-                    
-        return o
-    
-sol = Solution().maximumNumberOfStringPairs(words = ["aa","ab"])
+        pairs = 0
+        strings = set()
+        for word in words:
+            if word in strings:
+                pairs += 1
+            else:
+                strings.add(word[::-1])
+
+        return pairs
+
+
+sol = Solution().maximumNumberOfStringPairs(
+    words=["aa", "ab"])
 print(sol)
