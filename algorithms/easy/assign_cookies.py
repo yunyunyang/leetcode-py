1817518dc722a1b6925894034f1843de7facf01f
Chloe <yunyunya@gmail.com>
2024-01-02 15:54:36 +0100

    455. Assign Cookies

--- algorithms/easy/assign_cookies.py
+++ algorithms/easy/assign_cookies.py
@@ -8,11 +8,8 @@ class Solution:
         n, i, j = 0, len(g)-1, len(s)-1
         g.sort(reverse=True)
         s.sort(reverse=True)
-        print(g)
-        print(s)
         while i >= 0 and j >= 0:
             if s[j] >= g[i]:
-                print(s[j], g[j])
                 n += 1
                 i -= 1
                 j -= 1
