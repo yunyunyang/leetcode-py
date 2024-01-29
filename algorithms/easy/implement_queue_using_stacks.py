c81af669246e9b78c6ed98ed87ce3fdb13b8e0e1
Chloe <yunyunya@gmail.com>
2024-01-29 11:51:10 +0100

    232. Implement Queue using Stacks

---
+++ algorithms/easy/implement_queue_using_stacks.py
@@ -0,0 +1,28 @@
+# 232. Implement Queue using Stacks
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        head = self.stack[0]
+        self.stack = self.stack[1:]
+        return head
+
+    def peek(self) -> int:
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        return not self.stack
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
