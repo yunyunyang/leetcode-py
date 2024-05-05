1aa5c0917aa215fd284900bcd7cfd888acb74bba
Chloe <yunyunya@gmail.com>
2024-05-05 15:47:03 +0200

    237. Delete Node in a Linked List

---
+++ algorithms/easy/remove_element.py
@@ -0,0 +1,10 @@
+# 27. Remove Element
+
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        return
+    
+sol = Solution().removeElement()
+print(sol)
