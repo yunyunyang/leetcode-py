0de6cc4666b26a70f606414e850da76bc7bbb531
Chloe <yunyunya@gmail.com>
2024-05-15 17:08:50 +0200

    74. Search a 2D Matrix

--- algorithms/medium/search_a_2d_matrix.py
+++ algorithms/medium/search_a_2d_matrix.py
@@ -4,12 +4,19 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for r in matrix:
-            if target >= r[0] and target <= r[-1]:
-                for c in r:
-                    if c == target:
-                        return True
-                    
+        row, col = len(matrix), len(matrix[0])
+        left, right = 0, row * col - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            val = matrix[mid // col][mid % col]
+            print(mid, val)
+            if val == target:
+                return True
+            elif val < target:
+                left = mid + 1
+            elif val > target:
+                right = mid - 1
+                
         return False
     
 sol = Solution().searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13)
