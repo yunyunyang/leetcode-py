a8593aef2b9eba7d6ec08484c0cae35c121f1dc6
Chloe <yunyunya@gmail.com>
2024-05-16 16:45:18 +0200

    33. Search in Rotated Sorted Array

---
+++ algorithms/medium/search_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+# Search in Rotated Sorted Array
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+    
+# sol = Solution().search(nums = [5,1,3], target = 3)
+sol = Solution().search(nums = [6,7,0,1,2,4,5], target = 0)
+print(sol)
