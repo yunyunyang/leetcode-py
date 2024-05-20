2f65acd5a3df8a4a9c0d21e4e52019da1d780c1b
Chloe <yunyunya@gmail.com>
2024-05-20 12:45:22 +0200

    1863. Sum of All Subset XOR Totals

--- algorithms/easy/sum_of_all_subset_xor_totals.py
+++ algorithms/easy/sum_of_all_subset_xor_totals.py
@@ -4,18 +4,31 @@ from typing import List
 
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        r = [[]]
-        for i in nums:
-            r += [s + [i] for s in r]
-
-        o = 0
-        for i in range(1, len(r)):
-            t = r[i][0]
-            for j in range(1, len(r[i])):
-                t ^= r[i][j]
-            o += t
-
-        return o
+        all_subset, subset = [], []
+        def backtrack(i):
+            if i >= len(nums):
+                all_subset.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            backtrack(i + 1)
+            
+            subset.pop()
+            backtrack(i + 1)
+            
+        backtrack(0)
+        
+        output = 0
+        for subset in all_subset:
+            if len(subset) == 1:
+                output += subset[0]
+            elif len(subset) > 1:
+                x = subset[0]
+                for i in range(1, len(subset)):
+                    x ^= subset[i]
+                output += x
+        
+        return output
     
 sol = Solution().subsetXORSum(nums = [1,3])
 print(sol)
