74116a0d08d5782a56e59779c20692304dde6cfc
Chloe <yunyunya@gmail.com>
2024-06-03 11:27:21 +0200

    2486. Append Characters to String to Make Subsequence

---
+++ algorithms/medium/append_characters_to_string_to_make_subsequence.py
@@ -0,0 +1,8 @@
+# 2486. Append Characters to String to Make Subsequence
+
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        return
+    
+sol = Solution().appendCharacters(s = "coaching", t = "coding")
+print(sol)
