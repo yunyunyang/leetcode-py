681c9967d67518364234fae43591b6824e35f948
Chloe <yunyunya@gmail.com>
2024-06-03 23:21:04 +0200

    2486. Append Characters to String to Make Subsequence

--- algorithms/medium/append_characters_to_string_to_make_subsequence.py
+++ algorithms/medium/append_characters_to_string_to_make_subsequence.py
@@ -2,7 +2,16 @@
 
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        return
+        right = 0
+        
+        for left in range(len(s)):
+            if s[left] == t[right]:
+                right += 1
+            
+            if right >= len(t):
+                break
+                
+        return len(t) - right
     
 sol = Solution().appendCharacters(s = "coaching", t = "coding")
 print(sol)
