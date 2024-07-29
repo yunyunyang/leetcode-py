e1c5fc328e7e6aa4177825114f1a71610b86637d
Chloe <yunyunya@gmail.com>
2024-07-29 22:22:41 +0200

    202. Happy Number

--- algorithms/easy/happy_number.py
+++ algorithms/easy/happy_number.py
@@ -3,22 +3,20 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         visit = set()
+        
         while n not in visit:
             visit.add(n)
-            n = self.sumOfSquares(n)
+            s = 0
+            while n > 0:
+                digit = n % 10
+                s += digit ** 2
+                n //= 10
+            n = s
+            
             if n == 1:
                 return True
-
+            
         return False
-
-    def sumOfSquares(self, n: int) -> int:
-        o = 0
-        while n:
-            digit = n % 10
-            digit = digit ** 2
-            o += digit
-            n = n // 10
-        return o
 
 
 sol = Solution().isHappy(n=2)
