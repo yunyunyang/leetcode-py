f46054100b00cba6e45681910d391b042510eab8
Chloe <yunyunya@gmail.com>
2024-08-05 23:59:17 +0200

    57. Insert Interval

--- algorithms/medium/insert_interval.py
+++ algorithms/medium/insert_interval.py
@@ -5,20 +5,19 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        merge_intervals = []
+        output = []
+
         for i in range(len(intervals)):
             if newInterval[1] < intervals[i][0]:
-                merge_intervals.append(newInterval)
-                return merge_intervals + intervals[i:]
+                output.append(newInterval)
+                return output + intervals[i:]
             elif newInterval[0] > intervals[i][1]:
-                merge_intervals.append(intervals[i])
+                output.append(intervals[i])
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(
-                    newInterval[1], intervals[i][1])]
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
 
-        merge_intervals.append(newInterval)
-
-        return merge_intervals
+        output.append(newInterval)
+        return output
 
 
 sol = Solution().insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 5])
