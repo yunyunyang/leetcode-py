b9f59ed8737e40d3870366c6d4fb664e52ae60ea
Chloe <yunyunya@gmail.com>
2024-08-05 09:39:17 +0200

    2053. Kth Distinct String in an Array

--- algorithms/easy/kth_distinct_string_in_an_array.py
+++ algorithms/easy/kth_distinct_string_in_an_array.py
@@ -1,12 +1,15 @@
 # 2053. Kth Distinct String in an Array
 
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
-        count = {}
+        count = defaultdict(int)
         for c in arr:
             count[c] = count.get(c, 0) + 1
+
+        print(count)
             
         for key, value in count.items():
             if value == 1:
