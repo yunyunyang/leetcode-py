82415cbaf1954ddc3c1ee2ff051a175b50ef6727
Chloe <yunyunya@gmail.com>
2024-08-30 13:57:06 +0200

    88. Merge Sorted Array

--- algorithms/easy/merge_sorted_array.py
+++ algorithms/easy/merge_sorted_array.py
@@ -7,9 +7,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[m+i] = nums2[i]
+        last = m + n - 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
 
-        nums1.sort()
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n, last = n - 1, last - 1
     
 sol = Solution().merge(nums1 = [0], m = 0, nums2 = [1], n = 1)
