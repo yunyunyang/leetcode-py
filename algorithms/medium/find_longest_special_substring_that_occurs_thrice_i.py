98f6b4ad52592315a98bc72d6f550eeb2e06afe6
Chloe <yunyunya@gmail.com>
2024-12-10 19:34:49 +0100

    2981. Find Longest Special Substring That Occurs Thrice I

---
+++ algorithms/medium/find_longest_special_substring_that_occurs_thrice_i.py
@@ -0,0 +1,15 @@
+# 2981. Find Longest Special Substring That Occurs Thrice I
+
+def maximumLength(self, s: str) -> int:
+    count = {}
+    for i in range(len(s)):
+        for j in range(i + 1, len(s) + 1):
+            sub = s[i:j]
+            count[sub] = count.get(sub, 0) + 1
+
+    output = -1
+    for k, v in count.items():
+        if v >= 3 and len(set(k)) == 1:
+            output = max(output, len(k))
+
+    return output
