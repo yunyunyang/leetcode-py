2c64f05e581d7ae87b4807615294ed722bc49f69
Chloe <yunyunya@gmail.com>
2025-01-01 18:54:03 +0100

    1422. Maximum Score After Splitting a String

--- algorithms/easy/maximum_score_after_slitting_a_string.py
+++ algorithms/easy/maximum_score_after_slitting_a_string.py
@@ -6,10 +6,9 @@ def maxScore(self, s: str) -> int:
 
     for i in range(len(s) - 1):
         if s[i] == '0':
-            zero += 1
-            score = max(score, zero + one)
+            zero += 1            
         else:
             one -= 1
-            score = max(score, zero + one)
+        score = max(score, zero + one)
 
     return score
