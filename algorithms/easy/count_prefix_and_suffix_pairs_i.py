b6b0985d7d0f9efb3906fe378ac3c4764649be39
Chloe <yunyunya@gmail.com>
2025-01-08 10:32:51 +0100

    3042. Count Prefix and Suffix Pairs I

--- algorithms/easy/count_prefix_and_suffix_pairs_i.py
+++ algorithms/easy/count_prefix_and_suffix_pairs_i.py
@@ -5,9 +5,13 @@ from typing import List
 def countPrefixSuffixPairs(self, words: List[str]) -> int:
     count = 0
     for i in range(len(words)):
-        n = len(words[i])
         for j in range(i + 1, len(words)):
-            if words[j][0:n] == words[i] and words[j][len(words[j]) - n:len(words[j])] == words[i]:
+            str1 = words[i]
+            str2 = words[j]
+            if len(str1) > len(str2):
+                continue
+                
+            if str2.startswith(str1) and str2.endswith(str1):
                 count += 1
 
     return count
