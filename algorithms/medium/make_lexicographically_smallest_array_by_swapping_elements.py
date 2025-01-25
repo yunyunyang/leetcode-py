a7b672a3062609d7b622c36986ae0b33ef865783
Chloe <yunyunya@gmail.com>
2025-01-25 12:06:39 +0100

    2948. Make Lexicographically Smallest Array by Swapping Elements

---
+++ algorithms/medium/make_lexicographically_smallest_array_by_swapping_elements.py
@@ -0,0 +1,19 @@
+# 2948. Make Lexicographically Smallest Array by Swapping Elements
+
+from typing import List
+
+def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+    groups = []
+    num_of_group = {}
+    for n in sorted(nums):
+        if not groups or abs(n - groups[-1][-1]) > limit:
+            groups.append(deque())
+        groups[-1].append(n)
+        num_of_group[n] = len(groups) - 1
+    
+    res = []
+    for n in nums:
+        i = num_of_group[n]
+        res.append(groups[i].popleft())
+    
+    return res
